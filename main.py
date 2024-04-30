1071e7440747cdbb20eedb259662dda10795ce88
Revanth Krishna <revukrishna2000@gmail.com>
2024-04-30 16:28:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -89,6 +89,6 @@ def qa():
 #         return jsonify({"error": "User ID not provided"}), 400
     
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
 
 
